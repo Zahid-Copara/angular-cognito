7cde1280267f7704fffd32c3457f7370bbc7695f
Zahid Copara <zadcup2304@gmail.com>
2023-09-17 17:13:06 -0500

    master: Primer Commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: true,
+    cognito: {
+        userPoolId: 'pus-east-2_f8JkjgEZb',
+        userPoolWebClientId: '4eeqjajrac6ref5icc3tci2lva'
+    }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production: false,
+    cognito: {
+        userPoolId: 'pus-east-2_f8JkjgEZb',
+        userPoolWebClientId: '4eeqjajrac6ref5icc3tci2lva'
+    }
+};
